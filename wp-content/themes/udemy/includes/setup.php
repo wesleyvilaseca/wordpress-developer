b107bb9dddd27df8377e6c98c006172478080f76
wesley <wesley.vilaseca@gmail.com>
2022-03-15 17:52:03 -0300

    add theme support 'post-thumbnails' and loop to post blog

--- wp-content/themes/udemy/includes/setup.php
+++ wp-content/themes/udemy/includes/setup.php
@@ -2,6 +2,8 @@
 
 function ju_setup_theme()
 {
+    add_theme_support('post-thumbnails');
+    
     register_nav_menu('primary', __('Primary Menu', 'CodeVilaTheme'));
     register_nav_menu('secondary', __('Secondary Menu', 'CodeVilaTheme'));
 }
