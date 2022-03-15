036265803c50ef266008e7db61a89c7bc4c293d3
wesley <wesley.vilaseca@gmail.com>
2022-03-15 16:24:35 -0300

    dinamic sidebar on theme

--- wp-content/themes/udemy/functions.php
+++ wp-content/themes/udemy/functions.php
@@ -1,15 +1,26 @@
 <?php
 
-//setup
+/**
+ * setup
+ */
 define('JU_DEV_MODE', true);
 
-//includes
+/**
+ * includes
+ */
 include(get_theme_file_path('includes/front/enqueue.php'));
 include(get_theme_file_path('includes/setup.php'));
 include(get_theme_file_path('includes/custom-nav-walker.php'));
 
-//hooks
-add_action('wp_enqueue_scripts', 'ju_enqueue');
-add_action('after_setup_theme', 'ju_setup_theme');
+include(get_theme_file_path('includes/widgets.php'));
 
-//shortcodes
+/**
+ * hooks
+ */
+add_action('wp_enqueue_scripts',    'ju_enqueue');
+add_action('after_setup_theme',     'ju_setup_theme');
+add_action('widgets_init',          'ju_widgets');
+
+/**
+ * shortcodes
+ */
---
+++ wp-content/themes/udemy/includes/widgets.php
@@ -0,0 +1,14 @@
+<?php
+
+function ju_widgets()
+{
+    register_sidebar([
+        'name'              => __('my first theme sidebar', 'udemy'),
+        'id'                => 'ju_sidebar',
+        'description'       => __('Sidebar for the theme udemy', 'udemy'),
+        'before_widget'     => '<div id="%1$s" class="widget clearfix %2$s">',
+        'after_widget'      => '</div>',
+        'before_title'      => '<h4>',
+        'after_title'       => '</h4>' 
+    ]);
+}
