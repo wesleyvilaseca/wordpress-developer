86c064932ab343f78425d3d59f66ee1196bdcdc7
wesley <wesley.vilaseca@gmail.com>
2022-03-16 01:37:56 -0300

    script to show pages

--- wp-content/themes/codeVilaTheme/page.php
+++ wp-content/themes/codeVilaTheme/page.php
@@ -1,11 +1,34 @@
 <?php get_header() ?>
+<?php
+while (have_posts()) :
+    the_post();
+?>
+
+    <section class="page-title">
+        <div class="container clearfix">
+            <h1><?php the_title() ?></h1>
+            <span> <?php
+                    if (function_exists('the_subtitle')) {
+                        the_subtitle();
+                    }
+                    ?>
+            </span>
+        </div>
+    </section>
+
+<?php
+endwhile;
+rewind_posts();
+?>
+
+
 <section id="content">
 
     <div class="content-wrap">
         <div class="container clearfix">
             <div class="postcontent nobottommargin clearfix">
 
-               
+
             </div>
 
             <?php get_sidebar() ?>
