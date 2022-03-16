c902deefbcee5fe8f901c84338a68aea138d107f
wesley <wesley.vilaseca@gmail.com>
2022-03-16 00:53:43 -0300

    related posts in article

--- wp-content/themes/udemy/single.php
+++ wp-content/themes/udemy/single.php
@@ -103,20 +103,14 @@
                                 <div class="card-header">
                                     <strong>
                                         Posted by
-                                        <a href="#">John Doe</a>
+                                        <a href="<?php $author_url ?>"><?php the_author() ?></a>
                                     </strong>
                                 </div>
                                 <div class="card-body">
                                     <div class="author-image">
-                                        <img src="images/author/1.jpg" class="rounded-circle">
+                                        <?php echo get_avatar($author_id, 90, '', false, ['class' => 'img-circle']) ?>
                                     </div>
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                                    Dolores, eveniet, eligendi et nobis neque minus mollitia sit
-                                    repudiandae ad repellendus recusandae blanditiis praesentium v
-                                    itae ab sint earum voluptate velit beatae alias fugit
-                                    accusantium laboriosam nisi reiciendis deleniti tenetur
-                                    molestiae maxime id quaerat consequatur fugiat aliquam
-                                    laborum nam aliquid. Consectetur, perferendis?
+                                    <?php echo nl2br(get_the_author_meta('description')) ?>
                                 </div>
                             </div>
 
@@ -126,48 +120,50 @@
 
                             <div class="related-posts clearfix">
 
-                                <div class="mpost clearfix">
-                                    <div class="entry-image">
-                                        <a href="#">
-                                            <img src="images/blog/small/10.jpg">
-                                        </a>
-                                    </div>
-                                    <div class="entry-c">
-                                        <div class="entry-title">
-                                            <h4>
-                                                <a href="#">
-                                                    This is an Image Post
-                                                </a>
-                                            </h4>
-                                        </div>
-                                        <ul class="entry-meta clearfix">
-                                            <li><i class="icon-calendar3"></i> 10th July 2014</li>
-                                            <li><i class="icon-comments"></i> 12</li>
-                                        </ul>
-                                        <div class="entry-content">
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                                            elit. Mollitia nisi perferendis.
-                                        </div>
-                                    </div>
-                                </div>
+                                <?php
 
-                                <div class="mpost clearfix">
-                                    <div class="entry-image">
-                                        <a href="#"><img src="images/blog/small/20.jpg" alt="Blog Single"></a>
-                                    </div>
-                                    <div class="entry-c">
-                                        <div class="entry-title">
-                                            <h4><a href="#">This is a Video Post</a></h4>
-                                        </div>
-                                        <ul class="entry-meta clearfix">
-                                            <li><i class="icon-calendar3"></i> 24th July 2014</li>
-                                            <li><i class="icon-comments"></i> 16</li>
-                                        </ul>
-                                        <div class="entry-content">Lorem ipsum dolor sit amet, consectetur adipisicing
-                                            elit. Mollitia nisi perferendis.</div>
-                                    </div>
-                                </div>
+                                $categories = get_the_category();
+                                $rp_query = new WP_Query([
+                                    'posts_per_page'    => 2,
+                                    'post__not_in'      => [$post->ID], //the current post being viewd shold not be returned
+                                    'cat'               => !empty($categories) ? $categories[0]->term_id : null // define if the posts should be in a specific category
+                                ]);
 
+                                if ($rp_query->have_posts()) :
+                                    while ($rp_query->have_posts()) :
+                                        $rp_query->the_post();
+                                ?>
+                                        <div class="mpost clearfix">
+                                            <?php if (has_post_thumbnail()) : ?>
+                                                <div class="entry-image">
+                                                    <a href="<?php the_permalink(); ?>">
+                                                        <?php the_post_thumbnail('thumbnail'); ?>
+                                                    </a>
+                                                </div>
+                                            <?php endif; ?>
+
+                                            <div class="entry-c">
+                                                <div class="entry-title">
+                                                    <h4>
+                                                        <a href="<?php the_permalink() ?>">
+                                                            <?php the_title() ?>
+                                                        </a>
+                                                    </h4>
+                                                </div>
+                                                <ul class="entry-meta clearfix">
+                                                    <li><i class="icon-calendar3"></i> <? echo get_the_date() ?></li>
+                                                    <li><i class="icon-comments"></i> <?php comments_number('0') ?></li>
+                                                </ul>
+                                                <div class="entry-content">
+                                                    <?php the_excerpt() ?>
+                                                </div>
+                                            </div>
+                                        </div>
+                                <?php
+                                    endwhile;
+                                    wp_reset_postdata();
+                                endif;
+                                ?>
                             </div>
 
 
@@ -182,9 +178,6 @@
                     }
                 }
                 ?>
-
-
-
             </div>
 
             <?php get_sidebar() ?>
